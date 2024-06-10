4ac8ff9f3e7f497fb6a048214324f7adc70b3587
abrahmandevs <abrahmandevs@gmail.com>
2024-06-11 00:07:21 +0600

    Implement replace ticket data

--- app/Http/Controllers/Api/TicketController.php
+++ app/Http/Controllers/Api/TicketController.php
@@ -85,7 +85,32 @@ class TicketController extends ApiController
      */
     public function update(UpdateTicketRequest $request, $ticket_id)
     {
-        //ddf
+        // check user
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            $model = [
+                'title' => $request->input('data.attributes.title'),
+                'description' => $request->input('data.attributes.description'),
+                'status' => $request->input('data.attributes.status'),
+                'user_id' => $request->input('data.relationship.author.data.id'),
+            ];
+
+            $ticket->update($model);
+            return new TicketResource($ticket);
+
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot be found', 404);
+        }
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $request->input('data.relationship.author.data.id'),
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 
     /**
