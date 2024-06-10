77a4c6aace27ef0d67587474dbe9a11a4ada66ad
abrahmandevs <abrahmandevs@gmail.com>
2024-06-10 23:57:30 +0600

    Group api route

--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::middleware('auth:sanctum')->apiResource('tickets',TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('users',UserController::class);
-Route::middleware('auth:sanctum')->apiResource('users.tickets',AuthorTicketsController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tickets', TicketController::class)->except(['update']);
+    Route::put('tickets/{ticket}', [TicketController::class,'replace']);
+    
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('users.tickets', AuthorTicketsController::class);
+});
+
+
 
 // Route::middleware('auth:sanctum')->controller(TicketController::class)->group(function(){
 //     Route::get('/tickets','index')->name('tickets');
 //     Route::get('/tickets/{id}','show')->name('tickets.show');
 // });
-
-
