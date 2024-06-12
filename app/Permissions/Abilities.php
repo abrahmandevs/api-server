db6a0edbdecedda3228a9a0d26c1082959c185a9
abrahmandevs <abrahmandevs@gmail.com>
2024-06-12 07:49:04 +0600

    add ability to the user permission

---
+++ app/Permissions/Abilities.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Permissions;
+
+use App\Models\User;
+
+final class Abilities {
+    public const createTicket ='ticket:create';
+    public const UpdateTicket ='ticket:update';
+    public const ReplaceTicket ='ticket:replace';
+    public const DeleteTicket ='ticket:delete';
+
+    public const UpdateOwnTicket ='ticket:Own:update';
+    public const DeleteOwnTicket ='ticket:Own:delete';
+
+
+    public const createUser ='user:create';
+    public const UpdateUser ='user:update';
+    public const ReplaceUser ='user:replace';
+    public const DeleteUser ='user:delete';
+
+
+    public static function getAbilites(User $user){
+        if ($user->in_manager) {
+            return[
+                self::createTicket,
+                self::UpdateTicket,
+                self::ReplaceTicket,
+                self::DeleteTicket,
+                self::createUser,
+                self::UpdateUser,
+                self::ReplaceUser,
+                self::DeleteUser,
+            ];
+        }else{
+            return [
+                self::createTicket,
+                self::UpdateOwnTicket,
+                self::DeleteOwnTicket,
+
+            ];
+        }
+    }
+}
+
+?>
