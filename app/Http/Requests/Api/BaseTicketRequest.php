37416577e55b402122a3991f2acdb98128a7cc59
abrahmandevs <abrahmandevs@gmail.com>
2024-06-12 06:43:33 +0600

    make complete  BaseTicketRequest

--- app/Http/Requests/Api/BaseTicketRequest.php
+++ app/Http/Requests/Api/BaseTicketRequest.php
@@ -3,9 +3,30 @@
 namespace App\Http\Requests\Api;
 
 use Illuminate\Foundation\Http\FormRequest;
+use PhpParser\Node\Expr\FuncCall;
 
 class BaseTicketRequest extends FormRequest
 {
+    public function mappedAttributes(){
+        $attributeMap=[
+                'data.attributes.title'=>'title',
+                'data.attributes.description'=>'description',
+                'data.attributes.status'=>'status',
+                'data.relationalship.data.id'=>'user_id',
+                'data.attributes.createdAt'=>'created_at',
+                'data.attributes.updatedAt'=>'updated_at',
+        ];
+
+        $attrttributeToUpdate = [];
+        foreach ($attributeMap as $key => $attribute) {
+            if ($this->has($key)) {
+                $attrttributeToUpdate[$attribute]= $key;
+            }
+        }
+
+        return $attrttributeToUpdate;
+    }
+
     public function messages()
     {
         return [
